a7ee42a6e054db7a64071a5e1b5318e80cb8ca71
rokonmahmud <rokonudden6969@gmail.com>
2024-06-10 00:44:58 +0600

    add error element for 404 pages

--- src/Pages/Shared/Error/ErrorElement.jsx
+++ src/Pages/Shared/Error/ErrorElement.jsx
@@ -3,7 +3,7 @@ import errorimg from "../../../assets/images/home/error.jpg"
 const ErrorElement = () => {
     return (
         <div>
-            <img className="h-screen mx-auto" src={errorimg} alt="" />
+            {/* <img className="h-screen mx-auto" src={errorimg} alt="" /> */}
         </div>
     );
 };
